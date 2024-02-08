913a5482bd6304a45adeb1ea36e02239faf9ddc1
Arpenaige <Arpenaige@gmail.com>
2024-02-08 20:39:38 +0300

    test branch

--- TestAddingSubmoduleAsFile/Source.cpp
+++ TestAddingSubmoduleAsFile/Source.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
 	std::cout << "Hello, World!\n";
 
 	int c = 1;
+
+	std::string str1 = "branch test";
+	std::cout << str1 << '\n';
 
 	return 0;
 }
